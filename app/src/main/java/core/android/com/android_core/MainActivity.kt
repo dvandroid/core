69dfea458a705532b5722928d715dd9b39e02bfc
mmv <mvillaflor@yondu.com>
2019-01-02 10:25:28 +0800

    add changes

--- app/src/main/java/core/android/com/android_core/MainActivity.kt
+++ app/src/main/java/core/android/com/android_core/MainActivity.kt
@@ -1,12 +1,35 @@
 package core.android.com.android_core
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import core.android.com.corelib.permission.Permission
+import core.android.com.corelib.permission.PermissionCallback
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Permission.checkPermission(this, Manifest.permission.READ_CONTACTS) {
+            when (it) {
+                PermissionCallback.onNeedPermission -> {
+
+                }
+
+                PermissionCallback.onPermissionGranted -> {
+
+                }
+
+                PermissionCallback.onPermissionDisabled -> {
+
+                }
+
+                PermissionCallback.onPermissionPreviouslyDenied -> {
+
+                }
+            }
+        }
     }
 }
--- corelib/src/main/java/core/android/com/corelib/db/RoomModule.kt
+++
@@ -1,8 +0,0 @@
-package core.android.com.corelib.db
-
-import dagger.Module
-
-@Module
-class RoomModule {
-
-}
