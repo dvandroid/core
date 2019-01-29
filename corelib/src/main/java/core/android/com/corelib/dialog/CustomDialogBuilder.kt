9f677c91c90d8d837d499620711bf102f3b8ccab
donn_johnson_fabian <dfabian@yondu.com>
2019-01-29 09:15:31 +0800

    update default settings of custom dialog

--- corelib/src/main/java/core/android/com/corelib/dialog/CustomDialogBuilder.kt
+++ corelib/src/main/java/core/android/com/corelib/dialog/CustomDialogBuilder.kt
@@ -6,8 +6,8 @@ import io.github.jffiorillo.builder.JvmBuilder
 @JvmBuilder
 data class CustomDialogBuilder(val header : String? = null, val headerColor : Int = R.color.black,
                                val message : String, val messageColor : Int = R.color.text,
-                               val firstButton: String, val firstButtonTextColor : Int = R.color.black,
+                               val firstButton: String, val firstButtonTextColor : Int = R.color.white,
                                val firstButtonBackground : Int = R.drawable.default_button, var firstButtonAction : (() -> Unit)?,
-                               val secondButton: String? = null, val secondButtonTextColor : Int = R.color.black,
+                               val secondButton: String? = null, val secondButtonTextColor : Int = R.color.white,
                                val secondButtonBackground : Int = R.drawable.default_button, var secondButtonAction : (() -> Unit)?,
                                val isVertical: Boolean = false)
